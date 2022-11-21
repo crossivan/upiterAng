8886599b7ab72cfa988a2e722340d53462971254
cross <crossivan@yandex.ru>
2022-11-21 23:19:41 +0300

    Upiter start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { TachoPageComponent } from './pages/tacho-page/tacho-page.component';
+import { CeramPageComponent } from './pages/ceram-page/ceram-page.component';
+import { PhotodocPageComponent } from './pages/photodoc-page/photodoc-page.component';
+import { PrintPageComponent } from './pages/print-page/print-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    AboutPageComponent,
+    TachoPageComponent,
+    CeramPageComponent,
+    PhotodocPageComponent,
+    PrintPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
