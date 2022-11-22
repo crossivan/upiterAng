a521abe05571ba09fe6725b8bf149fb7bb8b4ffd
cross <crossivan@yandex.ru>
2022-11-22 20:33:31 +0300

    Form refactoring

--- src/app/pages/ceram-page/ceram-page.component.ts
+++ src/app/pages/ceram-page/ceram-page.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, UntypedFormGroup, Validators} from "@angular/forms";
 
-export interface Ceram {
-  id: number
-  firstName: string
-  lastName: string
-  patronymic: string
-  birthday: number
-  death: number
-}
-
 @Component({
   selector: 'app-ceram-page',
   templateUrl: './ceram-page.component.html',
@@ -27,43 +18,47 @@ export class CeramPageComponent implements OnInit {
   // }
 
   myForm: FormGroup
-  formData: FormGroup
-  textWidth: boolean = false
+  viewFormat: string = 'oval'
+  viewHole: boolean = true
   textClass: string = 'ceram__name'
 
   constructor() {
+
   }
 
-  changeInput($event: Event){
+  changeFormat(){
+    this.myForm.get('radioFormat')?.value == 1 ? this.viewFormat = 'oval' : this.viewFormat = 'rectangle'
+  }
+
+  changeHole(){
+    this.myForm.get('radioHole')?.value == 1 ? this.viewHole = true : this.viewHole = false
+  }
+
+  changeName(){
     let count: string = this.myForm.get('firstName')?.value + this.myForm.get('patronymic')?.value
 
     // count.length > 15 ? this.textClass = 'ceram__name--mod1' : this.textClass = "ceram__name"
-
     if (count.length > 15) {
-      this.textClass = 'ceram__name--mod1'
-      if (count.length > 21)  this.textClass = 'ceram__name--mod2'
+      this.textClass = 'textOnCeram__name--mod1'
+      if (count.length > 21)  this.textClass = 'textOnCeram__name--mod2'
     }
-    else this.textClass = 'ceram__name'
-
-
-    // const target = $event.target as HTMLInputElement
-    // const name = target.name
-
-    this.formData = {...this.myForm.value}
-
-    console.log(count.length)
-
+    else this.textClass = 'textOnCeram__name'
   }
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
+      radioFormat: new FormControl(1),
+      radioSize: new FormControl(1),
+      radioHole: new FormControl(1),
       lastName: new FormControl('Рогов', [Validators.required, Validators.minLength(4)]),
-      firstName: new FormControl('Иван'),
-      patronymic: new FormControl('Иванович')
+      firstName: new FormControl('Анатолий'),
+      patronymic: new FormControl('Ильич'),
+      birthday: new FormControl(''),
+      death: new FormControl('')
     })
     this.myForm.valueChanges.subscribe((status) => {
     //console.log(status)
-  })
+    })
   }
 
   submit() {
