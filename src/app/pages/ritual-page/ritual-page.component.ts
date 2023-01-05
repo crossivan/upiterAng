408dc295e1341cf829936af9551348bddbb5b0e1
crossivan <crossivan@yandex.ru>
2023-01-05 13:47:04 +0300

    Refact ceramic pages

--- src/app/pages/ritual-page/ritual-page.component.ts
+++ src/app/pages/ritual-page/ritual-page.component.ts
@@ -4,11 +4,11 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Title} from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-ceram-page',
-  templateUrl: './ceram-page.component.html',
-  styleUrls: ['./ceram-page.component.scss']
+  selector: 'app-ritual-page',
+  templateUrl: './ritual-page.component.html',
+  styleUrls: ['./ritual-page.component.scss']
 })
-export class CeramPageComponent implements OnInit, OnDestroy {
+export class RitualPageComponent implements OnInit, OnDestroy {
 
   constructor(private title: Title) {
 
@@ -26,16 +26,17 @@ export class CeramPageComponent implements OnInit, OnDestroy {
   viewFormat: string = 'oval'
   numFrame = 0
   viewHole: boolean = true
-  textClass: string = 'ceram__name'
+  textClass: string = 'textOnRitual__name'
   private changeFormatSubscription: Subscription | undefined
   private changeHoleSubscription: Subscription | undefined
 
   private initForm(): void {
     this.myForm =   new FormGroup({
-      radioFormat:  new FormControl(1),
-      radioHole:    new FormControl(1),
-      selectSize:   new FormControl(null),
-      withoutText:  new FormControl(false),
+      format:       new FormControl(1),
+      orientation:  new FormControl(1),
+      hole:         new FormControl(1),
+      size:         new FormControl(null),
+      withText:     new FormControl(false),
       withoutPhoto: new FormControl(false),
       lastName:     new FormControl(null, [
         Validators.required,
@@ -58,7 +59,7 @@ export class CeramPageComponent implements OnInit, OnDestroy {
   //   this.myForm.get('radioFormat')?.value == 1 ? this.viewFormat = 'assets/images/oval.png' : this.viewFormat = 'assets/images/rectangle2.png'
   // }
   private subscribeToChangeFormat(): void {
-    this.changeFormatSubscription = this.myForm.get('radioFormat')?.valueChanges.subscribe((value: number) => {
+    this.changeFormatSubscription = this.myForm.get('format')?.valueChanges.subscribe((value: number) => {
       value == 1
         ? this.viewFormat = 'oval'
         : this.viewFormat = 'rectangle'
@@ -66,7 +67,7 @@ export class CeramPageComponent implements OnInit, OnDestroy {
   }
 
   private subscribeToChangeHole(): void {
-    this.changeHoleSubscription = this.myForm.get('radioHole')?.valueChanges.subscribe((value: number) => {
+    this.changeHoleSubscription = this.myForm.get('hole')?.valueChanges.subscribe((value: number) => {
       value == 1 ? this.viewHole = true : this.viewHole = false
     })
   }
@@ -74,12 +75,12 @@ export class CeramPageComponent implements OnInit, OnDestroy {
   changeName(){
     let count: string = this.myForm.get('firstName')?.value + this.myForm.get('patronymic')?.value
 
-    // count.length > 15 ? this.textClass = 'ceram__name--mod1' : this.textClass = "ceram__name"
+    // count.length > 15 ? this.textClass = 'ritual__name--mod1' : this.textClass = "ritual__name"
     if (count.length > 15) {
-      this.textClass = 'textOnCeram__name--mod1'
-      if (count.length > 21)  this.textClass = 'textOnCeram__name--mod2'
+      this.textClass = 'textOnRitual__name--mod1'
+      if (count.length > 21)  this.textClass = 'textOnRitual__name--mod2'
     }
-    else this.textClass = 'textOnCeram__name'
+    else this.textClass = 'textOnRitual__name'
   }
 
   ngOnInit(): void {
